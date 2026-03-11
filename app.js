7abbff9e608e656d82b1d7b088434168c82fbd38
Johan Martinez <johangarciats2015@gmail.com>
2026-03-11 18:09:09 -0500

    Funciones de un API RESTful, Body-parser

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+
+const fs = require('fs');
+const path = require('path');
+const usersFilePath = path.join(__dirname, 'users.json');
+
+
 const app = express();
 const bodyParser = require('body-parser');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+function validateEmail(email) {
+    return emailRegex.test(email);
+}
 
 
 const PORT = process.env.PORT || 3000;
@@ -49,12 +59,86 @@ app.post('/form', ( req , res ) => {
 app.post('/api/data', ( req , res ) => {
     const  data = req.body;
     if (!data || Object.keys(data).legth === 0){
-        return req.status(400).json({error: 'No se recibieron los datos'});
+        return res.status(400).json({error: 'No se recibieron los datos'});
     }
     res.status(201).json({
         message: 'Datos json recibidos!',
         data
     })
+});
+
+app.get('/users', ( req , res ) => {
+    fs.readFile(usersFilePath, 'utf8', ( err , data ) =>{
+        if (err){
+            return res.status(500).json({error: 'Error con la base de datos'});
+        }
+        const users = JSON.parse(data);
+        res.json(users);
+    });
+});
+
+app.post('/users', ( req , res ) => {
+    const newUser = req.body;
+    if (!newUser.name || !newUser.email){
+        return res.status(400).json({error: 'Los campos son obligatorios'});
+    }
+    if (newUser.name.length < 3){
+        return res.status(400).json({error: 'El nombre es muy corto'});
+    }
+    if(!validateEmail(newUser.email)){
+        return res.status(400).json({error: 'El email no es válido'});
+    }
+    fs.readFile(usersFilePath, 'utf8', (err,data) => {
+        if(err){
+            return res.status(500).json({error: 'Error con el servidor'});
+        }
+        const users = JSON.parse(data);
+        users.push(newUser);
+        fs.writeFile(usersFilePath, JSON.stringify(users, null, 2), (err) => {
+            if(err){
+                return res.status(500).json({error: 'Error al guardar el usuario'});
+            }
+            res.status(201).json(newUser);
+        });
+    });
+});
+
+app.put('/users/:id', ( req , res ) => {
+    const userId = parseInt(req.params.id, 10);
+    const updatedUser = req.body;
+
+    fs.readFile(usersFilePath, 'utf8', ( err , data ) => {
+        if(err){
+            return res.status(500).json({error: 'Error con conexión de datos'});
+        }
+        let users = JSON.parse(data);
+        users = users.map(user => 
+            user.id === userId ? {...user, ...updatedUser} : user
+        );
+        fs.writeFile(usersFilePath, JSON.stringify(users, null , 2), (err) =>{
+            if(err){
+                return res.status(500).json({error: 'Error al actualizar el usuario'});
+            }
+            res.json(updatedUser);
+        });
+    });
+});
+
+app.delete('/users/:id', ( req , res ) => {
+    const userId = parseInt(req.params.id, 10);
+    fs.readFile(usersFilePath, 'utf8', (err , data) => {
+        if (err){
+            return res.status(500).json({error: 'Error con conexión de datos'});
+        }
+        let users = JSON.parse(data);
+        users = users.filter(user => user.id !== userId );
+        fs.writeFile(usersFilePath, JSON.stringify(users, null , 2), (err) => {
+            if (err){
+                return res.status(400).json({error: 'Error al eliminar un usuario.'});
+            }
+            res.status(204).send();
+        });
+    });
 });
 
 app.listen(PORT, () => {
