2bc56e0af9b4f47933d1a9cfd2f21b7a3106e7e4
Johan Martinez <johangarciats2015@gmail.com>
2026-03-11 13:48:08 -0500

    configuración del entorno de ExpressJs

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req,res) => {
-    res.send('Hello World!');
+    res.send(`
+        <h1>Curso de ExpressJS</h1>
+        <p>Esto es una aplicación NodeJS con ExpressJS</p>
+        <p>Corre en el puerto ${PORT}</p>
+        <p>Realizado por Sebastian Garcia <johangarciats2015@gmail.com> </p>
+    `);
 });
 
 app.listen(PORT, () => {
-    console.log('Nuestra aplicación esta funcionando!');
+    console.log(`Nuestra aplicación esta funcionando en el puerto ${PORT}`);
 });
+
