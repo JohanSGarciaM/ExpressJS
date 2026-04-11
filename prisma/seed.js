607b532651bca5e763b5fe726d27ffd3c0ffc721
Johan Martinez <johangarciats2015@gmail.com>
2026-04-10 21:39:04 -0500

    feeding prisma

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,14 +1,61 @@
+//const { PrismaClient } = require('@prisma/client');
 const { PrismaClient } = require('../generated/prisma');
 const prisma = new PrismaClient();
 
 async function main(){
-    await prisma.user.deleteMany();
+    //Creación Usuarios
+    //Usuario
+    const user1 = await prisma.user.create({
+        data: {
+            email: 'user1@example.com',
+            password: 'password123',
+            name: 'User One',
+            role: 'USER'
+        }
+    });
+    //Admin
+    const user2 = await prisma.user.create({
+        data: {
+            email: 'admin1@example.com',
+            password: 'admin123',
+            name: 'Admin One',
+            role: 'ADMIN'
+        }
+    });
+    // Crear bloques de tiempo
+    const timeBlock1 = await prisma.timeBlock.create({
+        data: {
+            startTime: new Date('2026-04-10T10:00:00Z'),
+            endTime: new Date('2026-04-10T11:00:00Z'),
+        }
+    });
+    const timeBlock2 = await prisma.timeBlock.create({
+        data: {
+            startTime: new Date('2026-04-10T11:00:00Z'),
+            endTime: new Date('2026-04-10T12:00:00Z'),
+        }
+    });
 
-    /// hacer las funciones para agregar usuarios
+    // Crear citas
+    await prisma.appointment.create({
+        data: {
+            date: new Date('2026-04-10T10:00:00Z'),
+            user: { connect: { id: user1.id}},
+            timeBlock: { connect: { id: timeBlock1.id}}
+        }
+    });
+    await prisma.appointment.create({
+        data: {
+            date: new Date('2026-04-10T11:00:00Z'),
+            user: { connect: { id: user2.id}},
+            timeBlock: { connect: { id: timeBlock2.id}}
+        }
+    });
 }
 main()
     .catch(e =>{
-        console.error(e)
+        console.error(e);
+        process.exit(1);
     })
     .finally(async () => {
         await prisma.$disconnect();
